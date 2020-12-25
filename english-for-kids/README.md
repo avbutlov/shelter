ac05e0eac40dbe6109bd40292c942797d70d7d93
avbutlov <butloff666@gmail.com>
2020-12-25 18:52:04 +0300

    relocate to another repo

--- english-for-kids/README.md
+++
@@ -1,2 +0,0 @@
-# avbutlov-JS2020Q3
-Private repository for @avbutlov
---
+++ shelter/slider.js
@@ -0,0 +1,45 @@
+if (window.matchMedia('(min-width: 1280px)').matches) {
+
+    function start_slick() {
+        $(".pet-slider").slick({
+            arrows: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+        });
+    }
+    setTimeout(start_slick, 1000);
+
+}
+
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+    function start_slick3() {
+        $(".pet-slider").slick({
+            arrows: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        });
+    }
+    setTimeout(start_slick3, 1000)
+
+}
+
+
+if (window.matchMedia('(max-width: 1279px)').matches) {
+    function start_slick2() {
+        $(".pet-slider").slick({
+            arrows: true,
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+        });
+    }
+    setTimeout(start_slick2, 1000)
+
+}
+
+
+
+
